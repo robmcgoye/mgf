d57fea78783a27953a43000bb97761769b1f5b72
Rob McGoye <rob@mcgoye.com>
2022-10-11 13:32:20 -0400

    Updated ahoy to set cloudflare managed transforms (country, lat/long, city)

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,13 +1,20 @@
 class Ahoy::Store < Ahoy::DatabaseStore
+  def track_visit(data)
+    data[:country] = request.headers["cf-ipcountry"]
+    data[:city] = request.headers["cf-ipcity"]
+    data[:latitude] = request.headers["cf-iplatitude"]
+    data[:longitude] = request.headers["cf-iplongitude"]
+    super(data)
+  end
 end
 
 # set to true for JavaScript tracking
 Ahoy.api = false
+Ahoy.visit_duration = 30.minutes
 
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = true
 
 # class Ahoy::Store < Ahoy::DatabaseStore
 #   def track_visit(data)
